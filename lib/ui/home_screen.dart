6d93ea254a2819e290cd1f50f26ad05cccd3ed50
Sharran14 <sharran.35@gmail.com>
2024-09-17 17:27:33 +0800

    UI Design Updated

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../bloc/gender_bloc.dart';
@@ -15,11 +17,11 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueGrey[50], // Set the background color of the screen
+      backgroundColor: const Color.fromARGB(255, 0, 15, 25), // Background color of the screen
       body: Center(
         child: Container(
           padding: EdgeInsets.all(16.0),
-          margin: EdgeInsets.all(20.0), // Adjust margin to create space around the white layer
+          margin: EdgeInsets.all(30.0), // Margin to create space around the white layer
           decoration: BoxDecoration(
             color: Colors.white, // White background for the layer
             borderRadius: BorderRadius.circular(12.0), // Rounded corners for the white layer
@@ -28,17 +30,31 @@ class _HomeScreenState extends State<HomeScreen> {
                 color: Colors.black.withOpacity(0.1),
                 spreadRadius: 4,
                 blurRadius: 8,
-                offset: Offset(0, 4), // Change the position of the shadow
+                offset: Offset(0, 4), // Position of the shadow
               ),
             ],
           ),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
-              Text(
-                "Enter a name to predict its gender.",
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+               Image.asset(
+                'assets/logo_gender.png', // Replace with the path to your logo image
+                height: 200, // Adjust the height as needed
+                width: 200,  // Adjust the width as needed
               ),
+              SizedBox(height: 20), 
+              // Text at the top of the white layer
+              Text(
+                "Gender Predictor",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 20), // Space between the title and the input field
+              
               Padding(
                 padding: const EdgeInsets.all(18.0),
                 child: TextField(
@@ -58,25 +74,66 @@ class _HomeScreenState extends State<HomeScreen> {
                 },
                 child: Text("Predict"),
               ),
-              SizedBox(height: 20),
+              SizedBox(height: 20), // Space between the button and the result
               BlocBuilder<GenderBloc, GenderState>(
-                builder: (context, state) {
-                  if (state is GenderInitial) {
-                    return Text("Enter a name to predict gender.");
-                  } else if (state is GenderLoading) {
-                    return CircularProgressIndicator();
-                  } else if (state is GenderLoaded) {
-                    return Text(
-                      "Gender: ${state.gender.toUpperCase()}, Probability: ${(state.probability * 100).toStringAsFixed(2)}%",
-                      style: TextStyle(fontSize: 16),
-                    );
-                  } else if (state is GenderError) {
-                    return Text(state.message, style: TextStyle(color: Colors.red));
-                  } else {
-                    return Container();
-                  }
-                },
+  builder: (context, state) {
+    if (state is GenderInitial) {
+      return Column(
+        children: [
+          SizedBox(height: 30), // Space to move the text lower
+          Text(
+            "Enter a name to predict gender.",
+            style: TextStyle(color: Colors.grey[700], fontSize: 16), // Update color
+            textAlign: TextAlign.center,
+          ),
+        ],
+      );
+    } else if (state is GenderLoading) {
+      return CircularProgressIndicator();
+    } else if (state is GenderLoaded) {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                "Gender: ",
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.black),
               ),
+              Text(
+                state.gender.toUpperCase(),
+                style: TextStyle(fontSize: 16, color: Colors.black),
+              ),
+            ],
+          ),
+          SizedBox(height: 10), // Space between rows
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                "Probability: ",
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.black),
+              ),
+              Text(
+                "${(state.probability * 100).toStringAsFixed(2)}%",
+                style: TextStyle(fontSize: 16, color: Colors.black),
+              ),
+            ],
+          ),
+        ],
+      );
+    } else if (state is GenderError) {
+      return Text(
+        state.message,
+        style: TextStyle(color: Colors.red),
+      );
+    } else {
+      return Container();
+    }
+  },
+)
+,
             ],
           ),
         ),
